b53d838baedccbdeb7a610de28f8db20bcff3b31
mayowa-kalejaiye <kalejaiyemayowa3@gmail.com>
2024-10-17 22:21:18 +0100

    Updated ALLOWED_HOSTS in settings.py and environment variables for deployment

--- mood_tracker/settings.py
+++ mood_tracker/settings.py
@@ -37,6 +37,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1,localhost,moodify-wmcd.onrender.com').split(',')
 
+
 print("ALLOWED_HOSTS:", ALLOWED_HOSTS)
 
 if not DEBUG:
